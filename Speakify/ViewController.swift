ff33067efb62a8172cbb9be891f2c9d421157ac0
dianafebrinalt <febrinalt123@gmail.com>
2021-07-26 22:51:53 +0700

    Initial Commit

---
+++ Speakify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speakify
+//
+//  Created by Diana Febrina Lumbantoruan on 26/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
